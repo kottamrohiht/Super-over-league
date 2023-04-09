02f7d2bf77d855ecc84d183ab479e01b8b649664
kottamrohith <kottamrohith123@gmail.com>
2023-04-09 18:41:41 +0000

    Super league

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="main-container">
+    <h1 className="heading">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
